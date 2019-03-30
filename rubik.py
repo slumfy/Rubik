853be543f7223f7a7eb0a68ed182912dca63c0f9
slumfy <valentin.robert.bransat@gmail.com>
2019-03-30 21:14:44 +0100

    gestion plus sensible des erreurs

--- rubik.py
+++ rubik.py
@@ -1,10 +1,11 @@
 import sys
 
-def error(info):
+def error(info, string):
   if info == 0:
     sys.stderr.write("bad arguments number\n")
   if info == 1:
-    sys.stderr.write("bad instruction format\n")
+    sys.stderr.write("bad instruction format ==>\t")
+    sys.stderr.write(string)
   sys.exit()
 
 def parsing(inst):
@@ -12,13 +13,13 @@ def parsing(inst):
   tab = inst.split(" ")
   for idx in range(len(tab)):
     if any(i in tab[idx][0] for i in db) == False:
-      error(1)
+      error(1, tab[idx])
     print("tableau d'instuction", idx,"\t",tab[idx])
 
 
 def main():
   if len(sys.argv) != 2:
-    error(0)
+    error(0, "0")
   parsing(sys.argv[1])
 
 main()
