7f25657498fd473b3e3cded6463ad3a2523eca2b
slumfy <valentin.robert.bransat@gmail.com>
2019-04-01 16:02:36 +0200

    création du rubik sous la forme de string

--- rubik.py
+++ rubik.py
@@ -1,4 +1,5 @@
 import sys
+rubik = "WWWWWWWWWBBBBBBBBBRRRRRRRRRGGGGGGGGGOOOOOOOOOYYYYYYYYY"
 
 def error(info, string):
   if info == 0:
@@ -10,12 +11,16 @@ def error(info, string):
 
 def parsing(inst):
   db = ["F","R","U","B","L","D"]
+  db2 = ["2", "'"]
   tab = inst.split(" ")
   for idx in range(len(tab)):
-    if any(i in tab[idx][0] for i in db) == False:
+    if len(tab[idx]) > 2:
+      error(1, tab[idx])
+    elif any(i in tab[idx][0] for i in db) == False:
+      error(1, tab[idx])
+    elif len(tab[idx]) == 2 and any(i in tab[idx][1] for i in db2) == False:
       error(1, tab[idx])
     print("tableau d'instuction", idx,"\t",tab[idx])
-
 
 def main():
   if len(sys.argv) != 2:
@@ -23,3 +28,4 @@ def main():
   parsing(sys.argv[1])
 
 main()
+print(len(rubik))
