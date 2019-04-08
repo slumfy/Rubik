f735cae67a3d05ebf14d63630e522e9488571e52
slumfy <valentin.robert.bransat@gmail.com>
2019-04-08 16:32:03 +0200

    fonction de verification du cube

--- rubik.py
+++ rubik.py
@@ -3,6 +3,7 @@ import parsing
 import print_rubik
 import move
 import shuffle
+import utils
 
 rubik =  ["Y","Y","Y","Y","Y","Y","Y","Y","Y",
           "O","O","O","O","O","O","O","O","O",
@@ -17,5 +18,9 @@ def main():
   inst = parsing.parsing(sys.argv[1])
   print_rubik.print_rubik(rubik)
   shuffle.shuffle(rubik, inst)
+  if utils.check_rubik(rubik) == False:
+    print("the cube is not solved")
+  else:
+    print("the cube is solved")
 
 main()
--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 def my_range(start, end, step):
-    while start <= end:
-        yield start
-        start += step
+	while start <= end:
+		yield start
+		start += step
 
 def swap(rubik,list):
     temp = rubik[list[0]]
@@ -9,3 +9,16 @@ def swap(rubik,list):
     rubik[list[1]] = rubik[list[2]]
     rubik[list[2]] = rubik[list[3]]
     rubik[list[3]] = temp
+
+def check_rubik(rubik):
+	valid =  ["Y","Y","Y","Y","Y","Y","Y","Y","Y",
+        	  "O","O","O","O","O","O","O","O","O",
+          	  "B","B","B","B","B","B","B","B","B",
+          	  "R","R","R","R","R","R","R","R","R",
+          	  "G","G","G","G","G","G","G","G","G",
+         	  "W","W","W","W","W","W","W","W","W"]
+			   
+	for idx in range(len(valid)):
+		if valid[idx] != rubik[idx]:
+			return (False)
+	return (True)
